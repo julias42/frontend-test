cb13b92f724811ed26960896f415a9ed1123cbae
julias42 <artyom-zhulikov@yandex.ru>
2023-08-22 15:19:30 +0700

    read script js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+//Функция с анимацией на блоки
+function mouseTransition (){
+  const block = document.querySelectorAll('.footer_blocks__big, \
+    .footer_blocks__block1, .footer_blocks__yellow, .footer_blocks__block2, \
+      .footer_blocks__block3, .footer_blocks__block4');
+    
+block.forEach(e => {
+      e.addEventListener('mouseenter', () => {
+        e.style.transition = 'all 1s ease-out';
+        e.style.transform = 'scale(1.05)';
+      })
+      e.addEventListener('mouseleave', ()=>{
+        e.style.transition = 'all 1s ease-out';
+        e.style.transform = '';
+      })
+    });
+  }
+
+  mouseTransition();
+
+//Функция с анимацией на скроллинг
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('footer_show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0, 0.5, 1] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.footer_blocks');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
